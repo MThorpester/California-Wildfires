72d5f19ca26a597c006d7fc2436b022674c9012e
Margaret Thorpe <mthorpester@gmail.com>
2021-04-21 14:39:13 -0700

    Added wildfire overlay layers to map for years 2013-2019.

--- Margaret/Map Stuff/static/js/logic.js
+++ Margaret/Map Stuff/static/js/logic.js
@@ -1,4 +1,4 @@
-function createMap(wildfireMarkers, Markers2019) {
+function createMap(wildfireMarkers, Markers2019, Markers2018, Markers2017, Markers2016, Markers2015, Markers2014, Markers2013) {
 
   // Create the outdoors tile layer that will be the background of our map
   var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -25,15 +25,22 @@ function createMap(wildfireMarkers, Markers2019) {
 
   // Create an overlayMaps object to hold the earthquakes layer
   var overlayMaps = {
-    "All Wildfires": wildfireMarkers,
-    "2019 Wildfires": Markers2019
+    "Other Wildfires": wildfireMarkers,
+    "2019 Wildfires": Markers2019,
+    "2018 Wildfires": Markers2018,
+    "2017 Wildfires": Markers2017,
+    "2016 Wildfires": Markers2016,
+    "2015 Wildfires": Markers2015,
+    "2014 Wildfires": Markers2014,
+    "2013 Wildfires": Markers2013
+
   };
- 
+   
   // Create the map object with options (center on Sacramento)
   var map = L.map("map-id", {
     center: [38.58, -121.49],
     zoom: 7,
-    layers: [outdoorsmap, wildfireMarkers, Markers2019]
+    layers: [outdoorsmap, wildfireMarkers, Markers2019, Markers2018, Markers2017, Markers2016, Markers2015, Markers2014, Markers2013]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
@@ -69,33 +76,78 @@ function createMarkers(response) {
   // Pull the features off of response.data
   var wildfire_data = response.features;
   console.log("wildfire is: ", wildfire_data);
+  console.log("Length of wildfire_data array: ",wildfire_data.length);
 
 //Initialize an array to hold earthquakes
    var wildfireMarkers = [];
    var Markers2019 = [];
+   var Markers2018 = [];
+   var Markers2017 = [];
+   var Markers2016 = [];
+   var Markers2015 = [];
+   var Markers2014 = [];
+   var Markers2013 = [];
+   var wildfireMarkerCount = 0;
   // Loop through the earthquakes array
   for (var index = 0; index < wildfire_data.length; index++) {
     var wildfire = wildfire_data[index];
-    // console.log("This wildfire is: ", wildfire);
+    // console.log("The type of the Archive Year is: ", typeof(wildfire.properties.ArchiveYear));
     // console.log("Latitude is: ", wildfire.geometry.coordinates[1]);
     // console.log("Longitude is: ", wildfire.geometry.coordinates[0]);
    
-    // For each earthquake, create a marker and bind a popup with the earthquake's place
+    // For each wildfire, create a marker and bind a popup with the earthquake's place
     var wildfireMarker = L.marker([wildfire.geometry.coordinates[1], wildfire.geometry.coordinates[0]])
-      .bindPopup("Name: " + wildfire.properties.Name + "<br>Latitude: " + wildfire.geometry.coordinates[1] + " Longitude: " + wildfire.geometry.coordinates[0]);
-    // Add the marker to the wildfireMarkers array
-      if (wildfire.properties.ArchiveYear == 2019) {
+      .bindPopup("Name: " + wildfire.properties.Name + "<br>Latitude: " + wildfire.geometry.coordinates[1] + " Longitude: " + wildfire.geometry.coordinates[0] +
+      "<br>Archived Year: " + wildfire.properties.ArchiveYear);
+    
+    // Add the marker to the array for the year the wildfire was recorded
+    switch(wildfire.properties.ArchiveYear) {
+      case 2019:
         console.log("2019 Marker: ",wildfire.properties.ArchiveYear);
         Markers2019.push(wildfireMarker);
-    } else {
-    wildfireMarkers.push(wildfireMarker);
-  }};
+        break;
+      case 2018:
+        console.log("2018 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2018.push(wildfireMarker);
+        break;
+      case 2017:
+        console.log("2017 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2017.push(wildfireMarker);
+        break;
+      case 2016:
+        console.log("2016 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2016.push(wildfireMarker);
+        break;
+      case 2015:
+        console.log("2015 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2015.push(wildfireMarker);
+        break;
+      case 2014:
+        console.log("2014 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2014.push(wildfireMarker);
+        break;
+      case 2013:
+        console.log("2013 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2013.push(wildfireMarker);
+        break;
+      default:
+        wildfireMarkers.push(wildfireMarker);
+        console.log("2015 Marker: ",wildfire.properties.ArchiveYear);
+    }
+
+
+  
+  // console.log("index value is ", index);
+};
    
   // console.log("earthquakeMarkers: ", earthquakeMarkers);
+  // console.log("Markers2013 after the Switch statement: ", Markers2013.length);
+
   
   // Create a layer group made from the wildfire marker arrays, pass it into the createMap function
-  createMap(L.layerGroup(wildfireMarkers),L.layerGroup(Markers2019));
-}
+  createMap(L.layerGroup(wildfireMarkers),L.layerGroup(Markers2019),L.layerGroup(Markers2018),
+  L.layerGroup(Markers2017),L.layerGroup(Markers2016), L.layerGroup(Markers2015), L.layerGroup(Markers2014), L.layerGroup(Markers2013));
+};
  
 
 
