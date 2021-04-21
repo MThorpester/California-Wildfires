9d2cd82357253e8f2ab29cbe08a27c1cde74436a
Margaret Thorpe <mthorpester@gmail.com>
2021-04-20 19:54:20 -0700

    Basic vanilla map set up - need to create layers for years next.

--- Margaret/Map Stuff/static/js/logic.js
+++ Margaret/Map Stuff/static/js/logic.js
@@ -1,28 +1,39 @@
-function createMap(wildfireMarkers) {
+function createMap(wildfireMarkers, Markers2019) {
 
-  // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // Create the outdoors tile layer that will be the background of our map
+  var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "light-v10",
+    id: "outdoors-v11",
     accessToken: API_KEY
   });
+//  changed from "light-v10"
+//   https://api.mapbox.com/styles/v1/mapbox/outdoors-v11.html?title=true&access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA#2/20/0
 
-  // Create a baseMaps object to hold the lightmap layer
+ // Create the street tile layer that will be the optional background of our map
+ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "streets-v11",
+  accessToken: API_KEY
+});
+  // Create a baseMaps object to hold the base map layers
   var baseMaps = {
-    "Light Map": lightmap
+    "Outdoors Map": outdoorsmap,
+    "Street Map": streetmap
   };
 
   // Create an overlayMaps object to hold the earthquakes layer
   var overlayMaps = {
-    "Wildfires": wildfireMarkers
+    "All Wildfires": wildfireMarkers,
+    "2019 Wildfires": Markers2019
   };
  
-  // Create the map object with options (center on Petrolia)
+  // Create the map object with options (center on Sacramento)
   var map = L.map("map-id", {
-    center: [40.32, -124.29],
-    zoom: 5,
-    layers: [lightmap, wildfireMarkers]
+    center: [38.58, -121.49],
+    zoom: 7,
+    layers: [outdoorsmap, wildfireMarkers, Markers2019]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
@@ -61,24 +72,29 @@ function createMarkers(response) {
 
 //Initialize an array to hold earthquakes
    var wildfireMarkers = [];
+   var Markers2019 = [];
   // Loop through the earthquakes array
   for (var index = 0; index < wildfire_data.length; index++) {
     var wildfire = wildfire_data[index];
-    console.log("This wildfire is: ", wildfire);
-    console.log("Latitude is: ", wildfire.geometry.coordinates[1]);
-    console.log("Longitude is: ", wildfire.geometry.coordinates[0]);
+    // console.log("This wildfire is: ", wildfire);
+    // console.log("Latitude is: ", wildfire.geometry.coordinates[1]);
+    // console.log("Longitude is: ", wildfire.geometry.coordinates[0]);
    
     // For each earthquake, create a marker and bind a popup with the earthquake's place
     var wildfireMarker = L.marker([wildfire.geometry.coordinates[1], wildfire.geometry.coordinates[0]])
-      .bindPopup("<h3><h3>Place: " + wildfire.properties.name + "</h3>");
-    // Add the marker to the bikeMarkers array
+      .bindPopup("Name: " + wildfire.properties.Name + "<br>Latitude: " + wildfire.geometry.coordinates[1] + " Longitude: " + wildfire.geometry.coordinates[0]);
+    // Add the marker to the wildfireMarkers array
+      if (wildfire.properties.ArchiveYear == 2019) {
+        console.log("2019 Marker: ",wildfire.properties.ArchiveYear);
+        Markers2019.push(wildfireMarker);
+    } else {
     wildfireMarkers.push(wildfireMarker);
-  }
-
+  }};
+   
   // console.log("earthquakeMarkers: ", earthquakeMarkers);
   
-  // Create a layer group made from the earthquake markers array, pass it into the createMap function
-  createMap(L.layerGroup(wildfireMarkers));
+  // Create a layer group made from the wildfire marker arrays, pass it into the createMap function
+  createMap(L.layerGroup(wildfireMarkers),L.layerGroup(Markers2019));
 }
  
 
@@ -93,7 +109,6 @@ var link = "static/data/WildfireIncidents.geojson";
 console.log("link is: ", link);
 // Get the wildfire GeoJSON file
 d3.json(link).then(function(data) {
-  console.log("Entering callback function");
-  console.log(data);
+  // console.log(data);
   createMarkers(data);
 });
